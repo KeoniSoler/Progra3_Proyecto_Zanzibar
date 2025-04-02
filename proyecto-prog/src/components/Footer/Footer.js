12d31792f3adcc90b4a803cf9b906a2ba65edf5f
salvadordelosheros <sdelosheros@udesa.edu.ar>
2025-04-02 15:58:50 -0300

    Eliminacion de archivos extra en src y Creacion del archivo Footer con su contenido

---
+++ proyecto-prog/src/components/Footer/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+import './styles.css'
+
+function Footer (){
+    return (
+        <footer>
+            <p>Desarrollado por: Keoni Soler y Salvador de los Heros</p>
+        </footer>
+    )
+}
+export default Footer
