4bc087b1bede44ea84fbea7063e2cc062e27c93e
salvadordelosheros <sdelosheros@udesa.edu.ar>
2025-04-09 19:14:03 -0300

    consfusion

--- proyecto-prog/src/screens/Detalle/Detalle.js
+++ proyecto-prog/src/screens/Detalle/Detalle.js
@@ -1,31 +1,31 @@
 import React, {Component} from "react";
 
 class Detalle extends Component{
-    constructor(props){
-        super(props)
-        this.state = {
-            peliculas: [],
-            backupPeliculas: [],
-        }
-    }
-    componentDidMount(){
-        fetch(`https://api.themoviedb.org/3/person/popular/${id}&api_key=<fb93fcd4664bbfe64f105075e91d8d7c>`)
-        .then((response) => response.json())
-        .then((data) => { 
-            this.setState({
-                peliculas: data.results, 
-                backupPeliculas: data.results
-            })
-        })
-        .catch((error) => console.log(error))
-    }
-    render(){
-        return (
-            <React.Fragment>
-                <h1>Detalle</h1>
-            </React.Fragment>
-        )
-    }
+   constructor(props){
+       super(props)
+       this.state = {
+           peliculas: [],
+           backupPeliculas: [],
+       }
+   }
+   componentDidMount(){
+       fetch(`https://api.themoviedb.org/3/person/popular/${id}&api_key=<fb93fcd4664bbfe64f105075e91d8d7c>`)
+       .then((response) => response.json())
+       .then((data) => {
+           this.setState({
+               peliculas: data.results,
+               backupPeliculas: data.results
+           })
+       })
+       .catch((error) => console.log(error))
+   }
+   render(){
+       return (
+           <React.Fragment>
+               <h1>Detalle</h1>
+           </React.Fragment>
+       )
+   }
 }
 
 export default Detalle;
